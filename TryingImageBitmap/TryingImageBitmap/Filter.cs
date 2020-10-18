a8350ba0a40f858fe05bcf7fd7cd22d6fecf8954
Alexander Nechaev <nechaev@sibgeoproject.ru>
2020-10-18 16:32:56 +0300

    разные варианты исполнения фильтрации

---
+++ TryingImageBitmap/TryingImageBitmap/Filter.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace TryingImageBitmap
+{
+    internal class Filter
+    {
+        private int _parts { get; set; }
+
+        private Bitmap _image { get; set; }
+
+        public Filter(int parts, Bitmap image)
+        {
+            _parts = parts;
+            _image = image;
+        }
+
+        public void Process()
+        {
+            Rectangle rect = new Rectangle(0, 0, _image.Width, _image.Height);
+            BitmapData bmpData = _image.LockBits(rect, ImageLockMode.ReadWrite, _image.PixelFormat);
+
+            FilterProcess(bmpData, _parts);
+        }
+
+        public async Task ProcessAsync()
+        {
+            Rectangle rect = new Rectangle(0, 0, _image.Width, _image.Height);
+            BitmapData bmpData = _image.LockBits(rect, ImageLockMode.ReadWrite, _image.PixelFormat);
+
+            await FilterProcessAsync(bmpData, _parts);
+        }
+
+        public void ProcessParallel()
+        {
+            Rectangle rect = new Rectangle(0, 0, _image.Width, _image.Height);
+            BitmapData bmpData = _image.LockBits(rect, ImageLockMode.ReadWrite, _image.PixelFormat);
+
+            FilterProcessParallel(bmpData, _parts);
+        }
+
+        private void FilterProcess(BitmapData data, int parts)
+        {
+            var rgbValues = new byte[Math.Abs(data.Stride) * _image.Height];
+            var lengthOfPart = Math.Abs(data.Stride) * _image.Height / parts;
+            Marshal.Copy(data.Scan0, rgbValues, 0, Math.Abs(data.Stride) * _image.Height);
+
+            //Создание параллельных задач
+            for (int i = 0; i < parts; i++)
+            {
+                var indexParam = i;
+                Implementation(rgbValues, indexParam, lengthOfPart);
+            }
+
+            Marshal.Copy(rgbValues, 0, data.Scan0, Math.Abs(data.Stride) * _image.Height);
+
+            _image.UnlockBits(data);
+        }
+
+        private void FilterProcessParallel(BitmapData data, int parts)
+        {
+            List<Action> actions = new List<Action>();
+            var rgbValues = new byte[Math.Abs(data.Stride) * _image.Height];
+            var lengthOfPart = Math.Abs(data.Stride) * _image.Height / parts;
+            Marshal.Copy(data.Scan0, rgbValues, 0, Math.Abs(data.Stride) * _image.Height);
+
+            //Создание параллельных задач
+            for (int i = 0; i < parts; i++)
+            {
+                var indexParam = i;
+                actions.Add(() => Implementation(rgbValues, indexParam, lengthOfPart));
+            }
+
+            //Вызов задач параллельно
+            Parallel.Invoke(actions.ToArray());
+            Marshal.Copy(rgbValues, 0, data.Scan0, Math.Abs(data.Stride) * _image.Height);
+
+            _image.UnlockBits(data);
+        }
+
+        private async Task FilterProcessAsync(BitmapData data, int parts)
+        {
+            //List<Action> actions = new List<Action>();
+            List<Task> tasks = new List<Task>();
+            var rgbValues = new byte[Math.Abs(data.Stride) * _image.Height];
+            var lengthOfPart = Math.Abs(data.Stride) * _image.Height / parts;
+            Marshal.Copy(data.Scan0, rgbValues, 0, Math.Abs(data.Stride) * _image.Height);
+
+            //Создание параллельных задач
+            for (int i = 0; i < parts; i++)
+            {
+                var indexParam = i;
+                Task t1 = Task.Run(() => Implementation(rgbValues, indexParam, lengthOfPart));
+                tasks.Add(t1);
+            }
+
+            await Task.WhenAll(tasks);
+            Marshal.Copy(rgbValues, 0, data.Scan0, Math.Abs(data.Stride) * _image.Height);
+
+            _image.UnlockBits(data);
+        }
+
+        private void Implementation(byte[] rgbValues, int index, int lengthOfPart)
+        {
+            //Console.WriteLine($"Выполняется задача {Task.CurrentId}");
+            //Console.WriteLine($"длина массива: {rgbValues.Length}");
+            //Console.WriteLine($"индекс: {index}");
+            //Console.WriteLine($"длина части: {lengthOfPart}");
+
+            //var counterCase = index * lengthOfPart + 2;
+            //var counterCase2 = (lengthOfPart * index) + lengthOfPart - 1;
+            //var length = rgbValues.Length;
+
+            for (int counter = index * lengthOfPart; counter < (lengthOfPart * index) + lengthOfPart - 1; counter += 3)
+                rgbValues[counter] = 255;
+        }
+    }
+}
--- TryingImageBitmap/TryingImageBitmap/Program.cs
+++ TryingImageBitmap/TryingImageBitmap/Program.cs
@@ -1,70 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Imaging;
-using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 
 namespace TryingImageBitmap
 {
     class Program
     {
-        private static Bitmap _bmp = new Bitmap("d:\\mountains-wallpapers.bmp");
+        private static Bitmap _image = new Bitmap("d:\\nigga.jpg");
 
-        static void Main()
+        static async Task Main()
         {
             Stopwatch stopWatch = new Stopwatch();
             stopWatch.Start();
-            ProcessImage(999);
-            _bmp.Save("d:\\mountains-wallpapers2.bmp", ImageFormat.Jpeg);
+            var filter = new Filter(3, _image);
+
+            stopWatch.Start();
+            filter.Process();
             stopWatch.Stop();
-            Console.WriteLine("затраченное время: " + stopWatch.ElapsedMilliseconds);
-        }
+            _image.Save("d:\\nigga1.jpg.", ImageFormat.Jpeg);
+            Console.WriteLine("затраченное время на синхронное выполнение: " + stopWatch.ElapsedMilliseconds);
 
-        private static void ProcessImage(int parts)
-        {
-            Rectangle rect = new Rectangle(0, 0, _bmp.Width, _bmp.Height);
-            BitmapData bmpData = _bmp.LockBits(rect, ImageLockMode.ReadWrite, _bmp.PixelFormat);
+            stopWatch.Reset();
 
-            FilterProcess(bmpData, parts);
-        }
+            stopWatch.Start();
+            await filter.ProcessAsync();
+            stopWatch.Stop();
+            _image.Save("d:\\nigga2.jpg", ImageFormat.Jpeg);
+            Console.WriteLine("затраченное время на асинхронное выполнение с паралельными задачами: " + stopWatch.ElapsedMilliseconds);
 
-        private static void FilterProcess(BitmapData data, int parts)
-        {
-            List<Action> actions = new List<Action>();
-            var rgbValues = new byte[Math.Abs(data.Stride) * _bmp.Height];
-            var lengthOfPart = Math.Abs(data.Stride) * _bmp.Height / parts;
-            Marshal.Copy(data.Scan0, rgbValues, 0, Math.Abs(data.Stride) * _bmp.Height);
+            stopWatch.Reset();
+
+            stopWatch.Start();
+            filter.ProcessParallel();
+            stopWatch.Stop();
+            _image.Save("d:\\nigga3.jpg", ImageFormat.Jpeg);
+            Console.WriteLine("затраченное время на параллельное выполнение(parallel): " + stopWatch.ElapsedMilliseconds);
             
-            //Создание параллельных задач
-            for (int i = 0; i < parts; i++)
-            {
-                var indexParam = i;
-                actions.Add(() => Filter(rgbValues, indexParam, lengthOfPart));
-                //Filter(rgbValues, indexParam, lengthOfPart);
-            }
-
-            //Вызов задач параллельно
-            Parallel.Invoke(actions.ToArray());
-            Marshal.Copy(rgbValues, 0, data.Scan0, Math.Abs(data.Stride) * _bmp.Height);
-
-            _bmp.UnlockBits(data);
-        }
-
-        private static void Filter(byte[] rgbValues, int index, int lengthOfPart)
-        {
-            Console.WriteLine($"Выполняется задача {Task.CurrentId}");
-            Console.WriteLine($"длина массива: {rgbValues.Length}");
-            Console.WriteLine($"индекс: {index}");
-            Console.WriteLine($"длина части: {lengthOfPart}");
-
-            var counterCase = index * lengthOfPart + 2;
-            var counterCase2 = (lengthOfPart * index) + lengthOfPart - 1;
-            var length = rgbValues.Length;
-
-            for (int counter = index * lengthOfPart + 2; counter < (lengthOfPart * index) + lengthOfPart-1; counter += 3)
-                rgbValues[counter] = 255;
+            Console.Read();
         }
     }
 }
